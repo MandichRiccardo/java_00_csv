7285394208dc993c08423feafb83e7a1a2f03d09
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-23 16:07:53 +0100

    aggiornamento 2023 12 23 16:07

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,29 @@
+import java.io.*;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String file = "src\\Personaggi.csv";
+        BufferedReader reader = null;
+        String line = "";
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine()) != null){
+                String[] row = line.split(",");
+                for(String index:row){
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
