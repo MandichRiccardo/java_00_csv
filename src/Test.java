b83406714966b44fda8e9ab49d43b67d9348f011
MandichRiccardo <ricky.mandich@gmail.com>
2023-12-24 17:46:11 +0100

    aggiornamento 2023 12 24 16:05 parseInt() completato

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,12 @@
 public class Test{
-
+    public static void main(String[] args) {
+        /*
+        new Csv().getCsv("src\\Personaggi.csv");
+        System.out.println("\n\n\n\n");
+         */
+        Parser p = new Parser();
+        String aString = "9";
+        int aInt = p.parseInt(aString);
+        System.out.print("String " + aString + ":\t" + aInt);
+    }
 }
